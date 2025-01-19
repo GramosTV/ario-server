7f7ad5967c54437dc7efc64becc75ae14622ae48
Mikołaj Gramowski <gramosmiki@onet.pl>
2025-01-19 16:18:05 +0100

    refactor(types): update enum values to use single quotes and add three-day timeframe

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,16 @@
 export enum Timeframe {
-    day = "Day",
-    week = "Week",
-    biweek = "2 Weeks",
-    month = "Month",
-    year = "Year",
-    lifetime = "Lifetime",
+  day = 'Day',
+  threeDay = '3 Days',
+  week = 'Week',
+  biweek = '2 Weeks',
+  month = 'Month',
+  year = 'Year',
+  lifetime = 'Lifetime',
 }
 export enum Status {
-    Undetected = "Undetected",
-    Updating = "Updating",
-    Soon = "Soon",
-    Testing = "Testing",
-    Down = "Down",
+  Undetected = 'Undetected',
+  Updating = 'Updating',
+  Soon = 'Soon',
+  Testing = 'Testing',
+  Down = 'Down',
 }
